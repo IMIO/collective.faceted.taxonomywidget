fcc7594cc665b5428126236a854017332876917a
Martin Peeters <martin.peeters@affinitic.be>
2016-08-02 16:48:48 +0200

    Preparing release 1.0a3

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@ Changelog
 =========
 
 
-1.0a3 (unreleased)
+1.0a3 (2016-08-02)
 ------------------
 
 - Add a taxonomy select2 widget.
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='collective.faceted.taxonomywidget',
-    version='1.0a3.dev0',
+    version='1.0a3',
     description="Faceted navigation taxonomy widget",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
